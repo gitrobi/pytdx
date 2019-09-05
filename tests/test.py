24d38d342bf2fbb6c5f22d11f7c388f93ef389d7
8118557@qq.com <8118557@qq.com>
2019-09-05 08:09:11 +0800

    added test.py added exquote.json, market.json, quote.json

--- tests/test.py
+++ tests/test.py
@@ -4,15 +4,25 @@ import time
 
 
 def test_hq():
+    import pprint
+
     api = TdxHq_API(multithread=False)
     with api.connect(time_out=30):
         for i in range(100):
-            stocks = api.get_security_quotes([(0, "000001"), (1, "600300")])
+            stocks = api.get_security_quotes([(0, "000001"), (1, "600993")])
             #print(stocks)
             #print(type(stocks))
-            print(stocks[0].get('code'), stocks[0].get('price'), stocks[0].get('bid1'),stocks[0].get('bid_vol1'), stocks[0].get('ask1'), stocks[0].get('ask_vol1'))
+            print(stocks[1].get('code'), stocks[1].get('price'), stocks[1].get('bid1'),stocks[1].get('bid_vol1'), stocks[1].get('ask1'), stocks[1].get('ask_vol1'))
+
+
+            #data = api.get_transaction_data(TDXParams.MARKET_SH, '601818', 0, 100)
+            #pprint.pprint(data)
+            #print('time:{},price:{},vol:{},buyorsell:{}'.format(data[0].get('time'), data[0].get('price'), data[0].get('vol'), data[0].get('buyorsell')))
+
             time.sleep(1.5)
 
+            # data = api.get_history_transaction_data(TDXParams.MARKET_SZ, '123001', 0, 10000, 20190430)
+            # print(data)
 
 def test_exhq():
     # symbol_params = [
@@ -38,5 +48,5 @@ def test_exhq():
             time.sleep(1.1)
 
 
-#test_hq()
-test_exhq()
+test_hq()
+#test_exhq()
